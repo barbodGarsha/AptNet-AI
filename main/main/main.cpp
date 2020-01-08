4aa137f16fc019f58649c061ff7f10b39b3d1afb
Barbod <Barbod@DESKTOP-S816Q3C>
2020-01-08 19:24:42 +0100

    finished neural_network_generator.h/cpp tested neural_network_generator.h/cpp it seems everything is working

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -6,7 +6,26 @@ int main()
 	NeuralNet nt;
 	nt = NeuralNet::NeuralNet("2,4,1,5");
 	nt.set_activations_funtions("0,0,0");
-	
+	std::cout << nt.input_index << std::endl;
+	std::cout << nt.hidden_index[0] << std::endl;
+	std::cout << nt.hidden_index[1] << std::endl;
+	std::cout << nt.output_index << std::endl;
+	MatrixXf i = MatrixXf(1, 2);
+	i(0, 0) = 1;
+	i(0, 1) = 0;
+	nt.set_inputs(i);
+	nt.feedforward();
+	std::cout << std::endl;
+	std::cout << nt.input_values << std::endl;
+
+	std::cout << std::endl;
+	std::cout << nt.hidden_values[0] << std::endl;
+
+	std::cout << std::endl;
+	std::cout << nt.hidden_values[1] << std::endl;
+
+	std::cout << std::endl;
+	std::cout << nt.output_values << std::endl;
 	system("pause");
 	return 0;
 }
